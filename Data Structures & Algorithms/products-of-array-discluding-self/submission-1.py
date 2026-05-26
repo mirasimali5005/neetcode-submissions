6ab43d68ed691405852bf6f40cfe9658926429e5
Mir Asim Ali <144566531+mirasimali5005@users.noreply.github.com>
2026-05-26 09:16:48 -0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [nums[1], nums[0]]
+        suffix = [nums[-1], nums[-2]]
+        pre = nums[1] * nums[0]
+        suf = nums[-1] * nums[-2]
+        for i in range(2, len(nums)):
+            prefix.append(pre)
+            pre = pre*nums[i]
+        for i in range(len(nums) - 3, -1, -1):
+            suffix = [suf] + suffix
+            suf = suf*nums[i]
+        for i in range(1, len(suffix)-1):
+            suffix[i] *= prefix[i]
+        suffix[-1] = prefix[-1]
+        return suffix
+
+        # using prefix and suffix we can get multiples from left and then multiples from right
+        # and then we can multiply them, the main thing to keep in mind is that the first and the
+        # the last element of the final answer is one of the first and last element of 
+        # prefix and suffix respectively so we shouldnt multiply the last elements 
+        
+        
+        
+
