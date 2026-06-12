ff6638410e61068c896c1f657624888c52e7f4f2
Mir Asim Ali <144566531+mirasimali5005@users.noreply.github.com>
2026-06-12 19:39:21 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        perm = {}
+        for letter in s1:
+            if letter in perm:
+                perm[letter] += 1
+            else:
+                perm[letter] = 1
+        
+        # s1 = "aabs"        s2 = "fsabsbaas"
+        #                    s2 = "fsabqbaas"
+        l = 0
+        r = 0
+        length = len(s1)
+        permtemp = {}
+        forwardlen = length
+        while r < len(s2):
+            if s2[r] in perm:
+                # window open
+                if forwardlen > 0:
+                    if s2[r] in permtemp:
+                        permtemp[s2[r]] += 1
+                    else:
+                        permtemp[s2[r]] = 1
+                    forwardlen -= 1
+                    if permtemp == perm:
+                        return True
+                    r += 1
+                else:
+                    if permtemp == perm:
+                        return True
+                    permtemp = {}
+                    forwardlen = length
+                    r = l + 1
+                    l = r
+                
+            else:
+                forwardlen = length
+                permtemp = {}
+                r += 1
+                l = r
+        return False
+        
+        
